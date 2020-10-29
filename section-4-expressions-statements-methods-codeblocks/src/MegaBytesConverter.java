817721d50071e09d847f7debb89e46b297857e35
mikhailshepelev <romanist-mike@yandex.ru>
2020-10-29 12:24:45 +0200

    section 4 is completed

---
+++ section-4-expressions-statements-methods-codeblocks/src/MegaBytesConverter.java
@@ -0,0 +1,18 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        int remainingKilobytes = kiloBytes;
+        int megaBytes = 0;
+
+        while (remainingKilobytes >= 1024) {
+            megaBytes++;
+            remainingKilobytes -= 1024;
+        }
+
+        System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKilobytes + " KB");
+    }
+}
---
+++ section-4-expressions-statements-methods-codeblocks/src/PlayingCat.java
@@ -0,0 +1,6 @@
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        return (summer && temperature >= 25 && temperature <= 45) ||
+                (!summer && temperature >= 25 && temperature <= 35);
+    }
+}
