a1c244712b50900855ef398609a20f58e6e40115
mikhailshepelev <romanist-mike@yandex.ru>
2021-03-22 20:52:39 +0200

    section 6 in progress

---
+++ section-6-oop-part-1-classes-constructors-and-inheritance/src/carpet_cost_exercise/Carpet.java
@@ -0,0 +1,13 @@
+package carpet_cost_exercise;
+
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost < 0 ? 0 : cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
