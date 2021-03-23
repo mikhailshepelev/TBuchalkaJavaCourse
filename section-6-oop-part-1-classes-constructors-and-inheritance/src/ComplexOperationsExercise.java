8dec27f7566e0dcf19b3408e6c3b41fd68d6a742
mikhailshepelev <romanist-mike@yandex.ru>
2021-03-23 19:55:13 +0200

    section 6 completed

---
+++ section-6-oop-part-1-classes-constructors-and-inheritance/src/ComplexOperationsExercise.java
@@ -0,0 +1,37 @@
+public class ComplexOperationsExercise {
+    private double real;
+    private double imaginary;
+
+    public ComplexOperationsExercise(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexOperationsExercise complexOperationsExercise) {
+        this.real += complexOperationsExercise.real;
+        this.imaginary += complexOperationsExercise.imaginary;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexOperationsExercise complexOperationsExercise) {
+        this.real -= complexOperationsExercise.real;
+        this.imaginary -= complexOperationsExercise.imaginary;
+    }
+}
